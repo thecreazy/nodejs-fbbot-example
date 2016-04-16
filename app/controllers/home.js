0cba8fae96b725521222709a6485ed835ddbd2b0
Riccardo Canella <canella_riccardo@icloud.com>
2016-04-16 19:19:41 +0200

    webhook api for fb verification

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -13,3 +13,10 @@ router.get('/', function (req, res, next) {
       articles: articles
     });
 });
+
+router.get('/webhook', function (req, res, next) {
+  if (req.query['hub.verify_token'] === 'CAAYs2ZCX4bPsBAIFzomk6DvZBnczQxTr9IRn5kotcZCJqzBlbFeNxet0O2ZB1HookjngJ0h4Rs1F0ZC3ldt1916oZCi2S1qmk75JCela5W676wk3HxzAOKoO3upU40Sp6YGSOx8SmhRCfP7tbZC17A6zr1ZAvHFI0Co9JZAZBTUNOmDDme1g3sFNssnoWZBXy2UUNMNmjYl39HWAQZDZD') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
