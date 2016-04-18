a7bb0643733f448307606c27b5226f00365dbf84
Riccardo Canella <canella_riccardo@icloud.com>
2016-04-18 10:34:30 +0200

    post webhook to receive the messagers

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,22 +1,60 @@
 var express = require('express'),
-  router = express.Router(),
-  Article = require('../models/article');
+    router = express.Router(),
+    request = require('request'),
+    Article = require('../models/article'),
+    token = "CAAYs2ZCX4bPsBAIFzomk6DvZBnczQxTr9IRn5kotcZCJqzBlbFeNxet0O2ZB1HookjngJ0h4Rs1F0ZC3ldt1916oZCi2S1qmk75JCela5W676wk3HxzAOKoO3upU40Sp6YGSOx8SmhRCfP7tbZC17A6zr1ZAvHFI0Co9JZAZBTUNOmDDme1g3sFNssnoWZBXy2UUNMNmjYl39HWAQZDZD";
 
-module.exports = function (app) {
-  app.use('/', router);
+module.exports = function(app) {
+    app.use('/', router);
 };
 
-router.get('/', function (req, res, next) {
-  var articles = [new Article(), new Article()];
+router.get('/', function(req, res, next) {
+    var articles = [new Article(), new Article()];
     res.render('index', {
-      title: 'Repo for fb bot example',
-      articles: articles
+        title: 'Repo for fb bot example',
+        articles: articles
     });
 });
 
-router.get('/webhook', function (req, res, next) {
-  if (req.query['hub.verify_token'] === 'CAAYs2ZCX4bPsBAIFzomk6DvZBnczQxTr9IRn5kotcZCJqzBlbFeNxet0O2ZB1HookjngJ0h4Rs1F0ZC3ldt1916oZCi2S1qmk75JCela5W676wk3HxzAOKoO3upU40Sp6YGSOx8SmhRCfP7tbZC17A6zr1ZAvHFI0Co9JZAZBTUNOmDDme1g3sFNssnoWZBXy2UUNMNmjYl39HWAQZDZD') {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
+router.get('/webhook', function(req, res, next) {
+    if (req.query['hub.verify_token'] === token) {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('Error, wrong validation token');
 });
+
+router.post('/webhook', function(req, res, next) {
+    var messaging_events = req.body.entry[0].messaging,
+        replayMessages = [];
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+        if (event.message && event.message.text) {
+            text = event.message.text;
+            sendTextMessage(sender, text.substring(0, 200));
+        }
+    }
+    res.sendStatus(200);
+});
+
+
+function sendTextMessage(sender, text) {
+    var messageData = {
+        text: text
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
